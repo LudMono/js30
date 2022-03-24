645bcb43b9aac4039f61e1fff6300754859459c3
LudMono <theblamno@gmail.com>
2022-03-24 10:56:19 +0300

    fear: 07 task completed

--- 07 - Array Cardio Day 2/app.js
+++ 07 - Array Cardio Day 2/app.js
@@ -1,28 +1,46 @@
 // ## Array Cardio Day 2
 
 const people = [
-  { name: 'Wes', year: 1988 },
-  { name: 'Kait', year: 1986 },
-  { name: 'Irv', year: 1970 },
-  { name: 'Lux', year: 2015 }
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
 ];
 
 const comments = [
-  { text: 'Love this!', id: 523423 },
-  { text: 'Super good', id: 823423 },
-  { text: 'You are the best', id: 2039842 },
-  { text: 'Ramen is my fav food ever', id: 123523 },
-  { text: 'Nice Nice Nice!', id: 542328 }
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
 ];
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const isAdult = (arr) =>
+  arr.some((el) => new Date().getFullYear() - el.year >= 19);
+
 // Array.prototype.every() // is everyone 19 or older?
+
+const isEveryAdult = (arr) =>
+  arr.every((el) => new Date().getFullYear() - el.year >= 19);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
+const findComment = (arr, id) => arr.find((el) => el.id === id);
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
+
+const deleteComment = (arr, id) => {
+  const el = arr.findIndex((el) => el.id === id);
+  const result = [...arr];
+  result.splice(el, 1);
+  return result;
+};
+
 // delete the comment with the ID of 823423
+
+console.log(deleteComment(comments, 823423));
